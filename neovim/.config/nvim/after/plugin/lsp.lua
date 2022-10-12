2e3c57bd2fcdba08d713b4e026627562abbd79f2
Hayden Johnson <hajohn100@gmail.com>
2022-10-12 09:07:00 -0700

    neovim python lsp

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -1,12 +1,24 @@
 local lspconfig = require('lspconfig')
 lspconfig.ccls.setup{}
+lspconfig.pylsp.setup{
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    maxLineLength = 88,
+                    ignore = {'E203', 'E501'},
+                    select = {'C', 'E', 'F', 'W', 'B', 'B950'}
+                },
+                black = {
+                    enabled = true
+                }
+            }
+        }
+    }
+}
 
 --Hover box
 vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
 
 --Jump to definition
 vim.api.nvim_set_keymap('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
-
---Omnifunc
-vim.api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
--- neovim/.config/nvim/after/plugin/python.lua
+++ neovim/.config/nvim/after/plugin/python.lua
@@ -1,24 +1,21 @@
-function c_setup()
+function python_setup()
     local set = vim.opt
     local setl = vim.opt_local
     local keymap = vim.api.nvim_set_keymap
     local re = { noremap = false }
     local nore = { noremap = true }
-    local rpath = "~/.config/nvim/templates/c"
+    local rpath = "~/.config/nvim/templates/python"
 
-    setl.textwidth = 80
-    setl.colorcolumn = "81"
     setl.omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    keymap("n", "<Leader>r", ':silent !cat Makefile | grep "INCLUDEDIR = " | sed -e "s/^.*= /clang\\n-I/" > .ccls<CR>:redraw!<CR>:LspRestart<CR>', nore)
-    keymap("i", ";;", '<ESC>A;', nore)
     keymap("n", "<Leader>l", '<cmd>lua vim.diagnostic.goto_next()<CR>', nore)
     keymap("n", "<Leader>h", '<cmd>lua vim.diagnostic.goto_prev()<CR>', nore)
+    keymap("n", "<Leader>f", '<cmd>lua vim.lsp.buf.formatting({ async = true })<CR>', nore)
 end
 
 vim.api.nvim_create_autocmd("Filetype", {
-    pattern = {"c", "cpp"},
+    pattern = "python",
     callback = function()
-        vim.schedule(c_setup)
+        vim.schedule(python_setup)
     end,
 })
