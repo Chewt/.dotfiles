98fd061dce0fc086d75549017c2a4e6324728bfc
Hayden Johnson <hajohn100@gmail.com>
2022-10-30 22:23:24 -0700

    changed ultinsips jump forward

--- neovim/.config/nvim/after/plugin/ultisnips.lua
+++ neovim/.config/nvim/after/plugin/ultisnips.lua
@@ -1,4 +1,4 @@
-vim.g.UltiSnipsExpandTrigger='<tab>'
-vim.g.UltiSnipsJumpForwardTrigger='<c-j>'
-vim.g.UltiSnipsJumpBackwardTrigger='<c-k>'
+vim.g.UltiSnipsExpandTrigger="<tab>"
+vim.g.UltiSnipsJumpForwardTrigger="<c-k>"
+vim.g.UltiSnipsJumpBackwardTrigger="<c-j>"
 vim.g.UltiSnipsSnippetDirectories={"UltiSnips", "my_snippets"}
