3fb5d6dbd949080baaef40587ffb5fdceca8abef
Hayden Johnson <hajohn100@gmail.com>
2022-11-01 18:56:17 -0700

    snippets and nvim config

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,24 @@
+local function on_attach(client, bufnr)
+  -- Enable completion triggered by <C-X><C-O>
+  -- See `:help omnifunc` and `:help ins-completion` for more information.
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- Use LSP as the handler for formatexpr.
+  -- See `:help formatexpr` for more information.
+  vim.api.nvim_buf_set_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+
+  --Hover box
+  vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
+
+  --Jump to definition
+  vim.api.nvim_set_keymap('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
+end
+
+
 local lspconfig = require('lspconfig')
-lspconfig.ccls.setup{}
+lspconfig.ccls.setup{
+    on_attach = on_attach
+}
 lspconfig.pylsp.setup{
     settings = {
         pylsp = {
@@ -15,11 +34,6 @@ lspconfig.pylsp.setup{
                 }
             }
         }
-    }
+    },
+    on_attach = on_attach
 }
-
---Hover box
-vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
-
---Jump to definition
-vim.api.nvim_set_keymap('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
