a2f5bc4fd7c13848cf0c462af0ec48b5c70ada76
Hayden Johnson <hajohn100@gmail.com>
2024-09-25 13:23:08 -0700

    Add new options

--- neovim/.config/nvim/lua/maps.lua
+++ neovim/.config/nvim/lua/maps.lua
@@ -1,11 +1,11 @@
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap("i", "{{", "<ESC>A<CR>{<CR>}<ESC>O", { noremap = true })
 vim.api.nvim_set_keymap("i", "((", "()<ESC>i", { noremap = true })
-vim.api.nvim_set_keymap("n", "<c-l>", ":bn<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<c-h>", ":bp<CR>", { noremap = true })
 vim.api.nvim_set_keymap("i", "<c-c>", "<ESC>", { noremap = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-function makefile_setup()
+local function makefile_setup()
     local keymap = vim.api.nvim_set_keymap
     local rpath = "~/.config/nvim/templates/"
     keymap("i", ";n", "<ESC>/(<>)<CR>cf)", { noremap = true })
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,20 +1,52 @@
-local Plug = vim.fn['plug#']
-vim.call('plug#begin', '~/.config/nvim/plugged')
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
--- This allows for installing LSP on the fly
-Plug 'williamboman/mason.nvim'
-Plug 'williamboman/mason-lspconfig.nvim'
-Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/nvim-cmp'     
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'L3MON4D3/LuaSnip'   
-Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v3.x'})
-Plug 'morhetz/gruvbox'
-Plug 'vim-airline/vim-airline'
-Plug 'lervag/vimtex'
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn['nvim-treesitter#TSUpdate']})
-Plug 'kaarmu/typst.vim'
-Plug 'mfussenegger/nvim-jdtls'
-Plug('akinsho/bufferline.nvim', { ['tag'] = '*' })
+local plugins = {
+    -- LSP stuff
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'L3MON4D3/LuaSnip',
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    'nvim-treesitter/nvim-treesitter',
+    'kaarmu/typst.vim',
+    'mfussenegger/nvim-jdtls',
+    -- Themes
+    'morhetz/gruvbox',
+    { "rose-pine/neovim", name = "rose-pine" },
+    -- AI
+    --'David-Kunz/gen.nvim',
+    {dir='~/Gits/forks/gen.nvim'},
+    -- UI changes
+    'nvim-lua/plenary.nvim',
+    {'nvim-telescope/telescope.nvim', version = '*'},
+    'nvim-telescope/telescope-ui-select.nvim',
+    'vim-airline/vim-airline',
+    -- Fun stuff
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    -- This is for git stuff
+    'tpope/vim-fugitive'
+}
 
-vim.call('plug#end')
+local opts = {}
+require('lazy').setup(plugins, opts)
