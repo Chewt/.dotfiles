56f03ea6cdfa62ee7878e86dda3d9a52907ef67d
Hayden Johnson <hajohn100@gmail.com>
2026-02-19 17:46:04 -0800

    remove lazy lock from neovim get history, and small changes

--- neovim/.config/nvim/lua/plugins/lsp_stuff.lua
+++ neovim/.config/nvim/lua/plugins/lsp_stuff.lua
@@ -83,17 +83,8 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
         build = ":TSUpdate",
-        config = function ()
-            local configs = require("nvim-treesitter.configs")
-
-            configs.setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
     },
     {'kaarmu/typst.vim',},
     {'ziglang/zig.vim',},
