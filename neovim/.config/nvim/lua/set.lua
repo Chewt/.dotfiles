03e4084d167f25bb48af2e40f6682902ad72cef5
Hayden Johnson <hajohn100@gmail.com>
2026-03-27 15:48:06 -0700

    Neovim: Add a few custom commands

--- neovim/.config/nvim/lua/set.lua
+++ neovim/.config/nvim/lua/set.lua
@@ -7,8 +7,6 @@ vim.opt_local.tw = 0
 vim.opt.foldenable = false
 vim.opt.hlsearch = false
 vim.opt.nu = true
-
--- I'm not sure I want to keep using relativenumbers
 vim.opt.rnu = true
 
 vim.opt.path:append('**')
@@ -16,3 +14,12 @@ vim.opt.wildmenu = true
 vim.opt.undofile = true
 
 vim.g.maplocalleader = ';'
+
+-- Run a command and place its output in a scratch buffer
+vim.cmd('com! -nargs=+ Run split | wincmd j | enew | setlocal buftype=nofile noswapfile bufhidden=wipe nobuflisted | resize 10 | silent -r! <args>')
+
+-- Set makeprg and run make in one go
+vim.cmd('com! -nargs=+ Make let &makeprg = "<args>" | make')
+
+-- Set makeprg temporarily, run make, then set it back
+vim.cmd('com! -nargs=+ Maket let oldmakeprg = &makeprg | let &makeprg = "<args>" | make | let &makeprg = oldmakeprg')
