27dcac213cbdaaa5c3f47cd443ff94abb44bd8a5
Hayden Johnson <hajohn100@gmail.com>
2026-04-03 12:51:18 -0700

    Add keybind to quickly copy things to system clipboard from visual mode

--- neovim/.config/nvim/lua/maps.lua
+++ neovim/.config/nvim/lua/maps.lua
@@ -3,6 +3,7 @@ vim.api.nvim_set_keymap("i", "{{", "<ESC>A<CR>{<CR>}<ESC>O", { noremap = true })
 vim.api.nvim_set_keymap("i", "((", "()<ESC>i", { noremap = true })
 vim.api.nvim_set_keymap("i", "<c-c>", "<ESC>", { noremap = true })
 vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
+vim.api.nvim_set_keymap("v", "<c-C>", "\"+y<ESC>", { noremap = true })
 
 local function makefile_setup()
     local keymap = vim.api.nvim_set_keymap
