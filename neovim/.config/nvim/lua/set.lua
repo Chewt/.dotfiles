54ceb62b0c8f6e7e549e8e2b06f621b3df4f0edc
Hayden Johnson <hajohn100@gmail.com>
2026-04-13 15:55:54 -0700

    Add custom Align function and command

--- neovim/.config/nvim/lua/set.lua
+++ neovim/.config/nvim/lua/set.lua
@@ -25,3 +25,36 @@ vim.cmd('com! -nargs=+ Make let &makeprg = "<args>" | make')
 
 -- Set makeprg temporarily, run make, then set it back
 vim.cmd('com! -nargs=+ Maket let oldmakeprg = &makeprg | let &makeprg = "<args>" | make | let &makeprg = oldmakeprg')
+
+function Align(range_start, range_end, pattern)
+    -- Default to '=' for pattern
+    pattern = (pattern ~= "" and pattern) or "="
+
+    -- Find max index of pattern
+    local maxIndexFound = 0
+    local lines = vim.api.nvim_buf_get_lines(0, range_start - 1, range_end, false)
+    for _, line in ipairs(lines) do
+        local matchIndex = string.find(line, pattern, 1, true)
+        if (matchIndex and matchIndex > maxIndexFound) then
+            maxIndexFound = matchIndex
+        end
+    end
+    for i, line in ipairs(lines) do
+        local matchIndex = string.find(line, pattern, 1, true)
+        if (matchIndex) then
+            lines[i] = string.sub(line, 1, matchIndex - 1) .. string.rep(" ", maxIndexFound - matchIndex) .. string.sub(line, matchIndex)
+        end
+    end
+    vim.api.nvim_buf_set_lines(0, range_start - 1, range_end, false, lines)
+end
+
+vim.api.nvim_create_user_command(
+    'Align',
+    function(opts)
+        Align(opts.line1, opts.line2, opts.fargs[1] or "")
+    end,
+    {
+        nargs = '?',
+        range = true
+    }
+)
