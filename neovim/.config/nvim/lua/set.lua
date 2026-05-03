1cd079bbb2d78c4275bc379d90fbfecdbf40e65e
Hayden Johnson <hajohn100@gmail.com>
2026-05-03 10:31:52 -0700

    Use spaces instead of tabs in neovim

--- neovim/.config/nvim/lua/set.lua
+++ neovim/.config/nvim/lua/set.lua
@@ -4,6 +4,7 @@ require('vim._core.ui2').enable()
 vim.opt.number = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.so = 5
 vim.opt_local.tw = 0
