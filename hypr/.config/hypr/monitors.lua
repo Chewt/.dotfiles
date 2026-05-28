39edfabd4d095b9579bcf349e2dbb92c63d06d6e
Hayden Johnson <hajohn100@gmail.com>
2026-05-27 20:42:08 -0700

    Remove files that change at runtime

--- hypr/.config/hypr/monitors.lua
+++
@@ -1,6 +0,0 @@
-hl.monitor({ output = "DP-1", mode = "2560x1440@179.95", position = "0x1080", scale = 1, })
-hl.monitor({ output = "DP-2", mode = "1920x1080@185.01", position = "2560x1285", scale = 1, })
-hl.monitor({ output = "DP-1", disabled = true })
-
--- For laptop
-hl.monitor({ output = "eDP-1", mode = "2256x1504@59.999", position = "0x0", scale = 1, })
