c57776b62de995aa23b93b638fd626e3e064b31a
Hayden Johnson <hajohn100@gmail.com>
2026-06-29 09:57:03 -0700

    Neovim: update markdown textwidth defaults

--- neovim/.config/nvim/after/ftplugin/markdown.lua
+++ neovim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
 vim.o.spell = true
-vim.o.textwidth = 120
+vim.o.textwidth = 80
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -161,5 +161,6 @@ vim.pack.add({
 require('oil').setup({
     view_options = {
         show_hidden = true,
-    }
+    },
+    skip_confirm_for_simple_edits = true
 })
